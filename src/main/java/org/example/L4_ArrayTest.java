af41dd2027dc0f2905a0d5176967c443db518c4f
bishnu <bishnu.das1191@gmail.com>
2025-03-05 07:52:58 +0530

    Added 1D and 2D array concepts

--- src/main/java/org/example/L4_ArrayTest.java
+++ src/main/java/org/example/L4_ArrayTest.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class L4_ArrayTest {
     public static void main(String[] args) {
-
+/*
         // 1D Array
 
         int[] arr; // declaration
@@ -40,6 +42,65 @@ public class L4_ArrayTest {
         // print all the values from array using while loop
 
         // print all the values from array using do while loop
+
+
+        */
+
+        int[] arr = {2,4,1,0,4,7,4,3};
+
+       /* for(int value : arr){
+            System.out.print(value+" ");
+        }*/
+
+        System.out.println("Before Sorting");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("After Sorting");
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        int[] numArr1 = {2,5,1,8};
+        int[] numArr2 = {2,5,1,8};
+        System.out.println(Arrays.equals(numArr1,numArr2));
+
+        int[] ar = new int[50];
+        System.out.println("Before Filling Elements");
+        System.out.println(Arrays.toString(ar));
+        Arrays.fill(ar,4);
+        System.out.println("After Filling Elements");
+        System.out.println(Arrays.toString(ar));
+
+        int[] ar11 = new int[3];  // [0,0,0]
+        ar11[0] = 2;
+        ar11[1] = 5;
+        ar11[2] = 8;
+
+
+
+        // 2D array
+        int[][] arr2;
+        int[][] ar2d = new int[3][3];
+
+        int[][] a2dArr = { {1,2,3}, {5,6,7}, {8,4,0} };
+
+        for(int[] outer : a2dArr){
+            for(int value : outer){
+                System.out.print(value+" ");
+            }
+            System.out.println();
+        }
+
+        // print 2d array elements using indexed based for loop
+
+        System.out.println(Arrays.deepToString(a2dArr));
+        System.out.println();
+
+
+
+
+
+
+
+
 
 
 
